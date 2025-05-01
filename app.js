166a829e990eaae74e6929109d7a1e11b1e2eceb
Beyza Zengin <beyzazengin76@gmail.com>
2025-05-01 19:22:46 +0300

    nodemon uploaded.

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express = require('express');
 
+const app = express();
 
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Photo Name',
+    description: 'Photo Description',
+  };
+
+  res.send(photo);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu port ${port} de başlatıldı.`);
+});
